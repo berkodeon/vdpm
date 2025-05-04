49a901107a5b67e0bd42b303272c509f50a91323
Berk Odeon <berkodeon@5a87b555-65a6-4b22-afbb-54817721403d.fritz.box>
2025-05-04 09:10:41 +0200

    Visidata spawn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let mut child = Command::new("vd")
+    .arg("~/Downloads/dedupe-example.csv")
+    .stdin(Stdio::inherit())
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()
+    .expect("failed to start VisiData");
+    // fetch the data and store it in private file
+    // visidata should respect the data and show properly
+    // watch the changes at this file
+    println!("Visidata started!");
+    child.wait().expect("VisiData process failed");
+    println!("Stopped Visidata");
+
 }
