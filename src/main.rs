726a3b57b6623765d138343285cf7acad6b36831
Berk Odeon <berkodeon@5a87b555-65a6-4b22-afbb-54817721403d.fritz.box>
2025-05-04 10:22:09 +0200

    Reading from config

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,26 @@ use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use directories::BaseDirs;
+use serde::Deserialize;
+use toml;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    settings: Settings,
+}
+
+#[derive(Debug, Deserialize)]
+struct Settings {
+    plugin_dir: String,
+    plugin_file: String,
+    plugin_folder: String,
+}
+
+fn read_config() -> Config {
+    let config_path = Path::new("config.toml");
+    let config_str = fs::read_to_string(config_path).expect("Failed to read config file");
+    toml::de::from_str(&config_str).expect("Failed to parse config file")
+}
 
 fn get_home_dir() -> PathBuf {
     if let Some(base_dirs) = BaseDirs::new() {
@@ -12,8 +32,9 @@ fn get_home_dir() -> PathBuf {
     }
 }
 
-fn create_plugin_directory(base_path: &PathBuf) -> PathBuf {
-    let plugin_dir = base_path.join(".config").join("vdpm");
+fn create_plugin_directory(plugin_dir_str: &str) -> PathBuf {
+    let home = get_home_dir();
+    let plugin_dir = home.join(plugin_dir_str);
 
     if !plugin_dir.exists() {
         fs::create_dir_all(&plugin_dir).expect("Failed to create plugin directory");
@@ -41,6 +62,8 @@ fn list_python_files(dir: &Path) -> Vec<String> {
 
 fn write_to_file(file_path: &str, content: &Vec<String>) -> io::Result<()> {
     let mut file = File::create(file_path)?;
+    writeln!(file, "plugin_name")?;
+
     for line in content {
         writeln!(file, "{}", line)?;
     }
@@ -48,21 +71,23 @@ fn write_to_file(file_path: &str, content: &Vec<String>) -> io::Result<()> {
 }
 
 fn main() {
-    let home_dir = get_home_dir();
-    let vdpm_plugin_dir = create_plugin_directory(&home_dir);
+    let config = read_config();
 
-    let plugin_path = home_dir.join(".visidata/plugins");
+    let plugin_dir = create_plugin_directory(&config.settings.plugin_dir);
+
+    let plugin_path = get_home_dir().join(&config.settings.plugin_folder);
     let python_files = list_python_files(&plugin_path);
 
-    let vdpm_plugins_file = vdpm_plugin_dir.join("plugins.csv").to_str().unwrap().to_string();
+    let plugins_file = plugin_dir.join(&config.settings.plugin_file);
+    let plugins_file_path = plugins_file.to_str().unwrap().to_string();
 
-    if let Err(e) = write_to_file(&vdpm_plugins_file, &python_files) {
+    if let Err(e) = write_to_file(&plugins_file_path, &python_files) {
         eprintln!("Error writing to file: {}", e);
         return;
     }
 
     let mut child = Command::new("vd")
-        .arg(&vdpm_plugins_file)
+        .arg(&plugins_file_path)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
