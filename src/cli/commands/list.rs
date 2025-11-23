120c357a45bb551fec25cdf0141f40c34aa34f45
Berk Odeon <berkodeon@MacBookPro.fritz.box>
2025-11-23 08:14:41 +0100

    Minor fix

--- src/cli/commands/list.rs
+++ src/cli/commands/list.rs
@@ -4,5 +4,9 @@ use tracing::info;
 
 pub async fn execute(config: &AppConfig) -> Result<()> {
     info!("Listing all installed plugins!");
+    // read the plugins folder
+    // read the visidatarc
+    // create a list of classes
+    // return
     Ok(())
 }
