b7281dec81f22410b522dfa598c339b7c20a2df2
wwdd1 <mhmtctn35@gmail.com>
2026-03-15 16:11:36 +0300

    VDPM-0000: more tests for registry

--- src/core/plugin.rs
+++ src/core/plugin.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use tabled::Tabled;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Tabled, Hash)]
+#[derive(Debug, Clone, Serialize, Deserialize, Tabled, Hash, PartialEq)]
 pub struct Plugin {
     pub name: String,
     pub enabled: bool,
